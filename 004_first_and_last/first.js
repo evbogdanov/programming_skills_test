0fea4553bcef35353ef11b8dd17dfed276c3da19
Ev Bogdanov <ev@bogdanov.io>
2017-02-20 17:28:38 +0300

    js 004

---
+++ 004_first_and_last/first.js
@@ -0,0 +1,28 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+	input:    process.stdin,
+	output:   process.stdout,
+	terminal: false
+})
+
+rl.on('line', line => {
+	const num = parseInt(line, 10)
+	if(isNaN(num)) {
+		throw new Error(`Invalid num: ${line}`)
+	}
+
+	const
+		strSqr = `${num * num}`,
+		len    = strSqr.length
+
+	if (len === 1) {
+		console.log(strSqr)
+	}
+	else {
+		const
+			firstDigit = parseInt(strSqr[0], 10),
+			lastDigit  = parseInt(strSqr[len-1], 10)
+		console.log(firstDigit + lastDigit)
+	}
+})
